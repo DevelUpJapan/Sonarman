fd63f60bad293ad81e4d6368db5d2ce497d7da50
DevelUpJapan <json@develup-japan.co.jp>
2016-03-04 11:41:40 +0900

    Update nav.php
    bugfix

--- sonarman/nav.php
+++ sonarman/nav.php
@@ -22,8 +22,8 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     }
 }
 ?>
-<!DOCTYPE sonarman>
-<sonarman>
+<!DOCTYPE html>
+<html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
